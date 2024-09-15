5a2ae6331c498c124098e97dd81ccea275135962
minic <minicov8@gmail.com>
2024-09-15 22:01:02 +1000

    Поправки для exe файла

--- PyCutieMerge/src/main.py
+++ PyCutieMerge/src/main.py
@@ -1,18 +1,23 @@
 import logging
 import os
+import sys
+import shutil
+import subprocess
 
 from API_Handler import APIHandler
 from Downloader import download_video, download_audio, download_subs
 from Merge import merge_video_audio_subs, cleanup_temp_files
 from Episode_Selector import EpisodeSelector
 
-logging.basicConfig(filename='mlp_downloader.log', level=logging.INFO,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
-                    encoding='utf-8')
+                    handlers=[
+                        logging.StreamHandler(sys.stdout)  # Вывод логов в консоль
+                    ])
 
-DOWNLOADS_DIR = os.path.join(os.path.expanduser("~"), "Downloads")
-TEMP_DIR = os.path.join(DOWNLOADS_DIR, ".temp")
-OUTPUT_DIR = os.path.join(DOWNLOADS_DIR, "CutieMerge")
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMP_DIR = os.path.join(SCRIPT_DIR, ".temp")
+OUTPUT_DIR = os.path.join(SCRIPT_DIR, "CutieMerge")
 
 if not os.path.exists(TEMP_DIR):
     os.makedirs(TEMP_DIR)
@@ -21,41 +26,50 @@ if not os.path.exists(OUTPUT_DIR):
 
 
 def main():
-    api = APIHandler()
+    try:
+        api = APIHandler()
 
-    if not api.check_api_availability():
-        return
+        if not api.check_api_availability():
+            input("Нажмите Enter для выхода...")
+            return
 
-    selector = EpisodeSelector(api)
-    season, selected_episode, english_title, selected_quality, selected_dub, selected_subs = selector.select_episode()
+        selector = EpisodeSelector(api)
+        season, selected_episode, english_title, selected_quality, selected_dub, selected_subs = selector.select_episode()
 
-    if not season or not selected_episode or not english_title or not selected_quality or not selected_dub:
-        return
+        if not season or not selected_episode or not english_title or not selected_quality or not selected_dub:
+            input("Нажмите Enter для выхода...")  # Ожидание ввода перед закрытием
+            return
 
-    if not download_video(season, selected_episode['localId'], selected_quality):
-        print("Ошибка при скачивании видео.")
-        return
+        if not download_video(season, selected_episode['localId'], selected_quality):
+            print("Ошибка при скачивании видео.")
+            return
 
-    if not download_audio(season, selected_episode['localId'], selected_dub['code']):
-        print("Ошибка при скачивании аудио.")
-        return
+        if not download_audio(season, selected_episode['localId'], selected_dub['code']):
+            print("Ошибка при скачивании аудио.")
+            return
 
-    if selected_subs and not download_subs(season, selected_episode['localId'], selected_subs['code']):
-        print("Ошибка при скачивании субтитров.")
-        return
+        if selected_subs and not download_subs(season, selected_episode['localId'], selected_subs['code']):
+            print("Ошибка при скачивании субтитров.")
+            return
 
-    video_filename = os.path.join(TEMP_DIR, "video.mp4") if int(selected_quality) <= 1080 else os.path.join(TEMP_DIR,
-                                                                                                           "video.webm")
-    audio_filename = os.path.join(TEMP_DIR, "audio.opus")
-    subs_filename = os.path.join(TEMP_DIR, "subs.ass") if selected_subs else None
-    output_filename = os.path.join(OUTPUT_DIR, f"{selected_episode['title']}.mkv")
+        video_filename = os.path.join(TEMP_DIR, "video.mp4") if int(selected_quality) <= 1080 else os.path.join(TEMP_DIR,
+                                                                                                               "video.webm")
+        audio_filename = os.path.join(TEMP_DIR, "audio.opus")
+        subs_filename = os.path.join(TEMP_DIR, "subs.ass") if selected_subs else None
+        output_filename = os.path.join(OUTPUT_DIR, f"{selected_episode['title']}.mkv")
 
-    if not merge_video_audio_subs(video_filename, audio_filename, subs_filename, output_filename):
-        print("Ошибка при объединении видео.")
-        return
+        if not merge_video_audio_subs(video_filename, audio_filename, subs_filename, output_filename):
+            print("Ошибка при объединении видео.")
+            return
 
-    cleanup_temp_files()
+        cleanup_temp_files()
 
+        print(f"\nСерия {selected_episode['title']} успешно скачана и объединена!\n"
+                           f"Ты можешь найти ее в папке {OUTPUT_DIR}")
+
+    except Exception as e:
+        logging.error(f"Произошла ошибка: {e}")
+    input("Нажмите Enter для выхода...")
 
 if __name__ == "__main__":
     main()
