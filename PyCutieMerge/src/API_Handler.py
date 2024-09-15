f99a6011640181dd8a487f3dd99a918088723f11
minic <minicov8@gmail.com>
2024-09-15 21:59:37 +1000

    Добавлены заголовки

--- PyCutieMerge/src/API_Handler.py
+++ PyCutieMerge/src/API_Handler.py
@@ -8,7 +8,24 @@ class APIHandler:
 
     def check_api_availability(self):
         try:
-            response = requests.get(API_BASE_URL)
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Connection': 'keep-alive',
+                'Upgrade-Insecure-Requests': '1',
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1'
+            }
+            response = requests.get(API_BASE_URL, headers=headers)
+            response.encoding = 'utf-8'
+            logging.info(f"Отправлен запрос: {response.request.url}")
+            logging.info(f"Получен ответ: {response.status_code} {response.reason}")
             response.raise_for_status()
             return True
         except requests.RequestException as e:
