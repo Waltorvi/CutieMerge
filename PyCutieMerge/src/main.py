102a6fdf6e4d65bfd7e8ba422d6ce80fd8bad750
minic <minicov8@gmail.com>
2024-09-17 22:58:41 +1000

    Логотип, цветное логирование, небольшие исправления

--- PyCutieMerge/src/main.py
+++ PyCutieMerge/src/main.py
@@ -3,11 +3,15 @@ import os
 import sys
 import shutil
 import subprocess
+from pathlib import Path
+from colorama import init, Fore, Style
 
 from API_Handler import APIHandler
 from Downloader import download_video, download_audio, download_subs
 from Merge import merge_video_audio_subs, cleanup_temp_files
 from Episode_Selector import EpisodeSelector
+
+init()
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -15,9 +19,24 @@ logging.basicConfig(level=logging.INFO,
                         logging.StreamHandler(sys.stdout)  # Вывод логов в консоль
                     ])
 
+class ColoredConsoleHandler(logging.StreamHandler):
+    def emit(self, record):
+        if record.levelno == logging.ERROR:
+            record.msg = Fore.RED + record.msg + Style.RESET_ALL
+        elif record.levelno == logging.INFO:
+            record.msg = Fore.YELLOW + record.msg + Style.RESET_ALL
+        super().emit(record)
+
+# Замена стандартного обработчика на раскрашенный
+for handler in logging.root.handlers[:]:
+    if isinstance(handler, logging.StreamHandler):
+        logging.root.removeHandler(handler)
+logging.getLogger().addHandler(ColoredConsoleHandler())
+
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+DOWNLOADS_DIR = str(Path.home() / "Downloads")
 TEMP_DIR = os.path.join(SCRIPT_DIR, ".temp")
-OUTPUT_DIR = os.path.join(SCRIPT_DIR, "CutieMerge")
+OUTPUT_DIR = os.path.join(DOWNLOADS_DIR, "CutieMerge")
 
 if not os.path.exists(TEMP_DIR):
     os.makedirs(TEMP_DIR)
@@ -27,49 +46,63 @@ if not os.path.exists(OUTPUT_DIR):
 
 def main():
     try:
+        init()
+        print(Fore.MAGENTA + " _______                _         _______                             " + Style.RESET_ALL)
+        print(Fore.MAGENTA + "(_______)          _   (_)       (_______)                            " + Style.RESET_ALL)
+        print(Fore.MAGENTA + " _        _   _  _| |_  _  _____  _  _  _  _____   ____   ____  _____ " + Style.RESET_ALL)
+        print(Fore.MAGENTA + "| |      | | | |(_   _)| || ___ || ||_|| || ___ | / ___) / _  || ___ |" + Style.RESET_ALL)
+        print(Fore.MAGENTA + "| |_____ | |_| |  | |_ | || ____|| |   | || ____|| |    ( (_| || ____|" + Style.RESET_ALL)
+        print(Fore.MAGENTA + " \______)|____/    \__)|_||_____)|_|   |_||_____)|_|     \___ ||_____)" + Style.RESET_ALL)
+        print(Fore.MAGENTA + "                                                        (_____|  " + Style.RESET_ALL)
+        print(
+            Fore.MAGENTA + "Сделано " + Fore.CYAN + "Waltorvi" + Fore.MAGENTA + " для " + Fore.WHITE + "МагияДружбы.рф" + Style.RESET_ALL)
+
         api = APIHandler()
 
+        ColoredConsoleHandler()
+
         if not api.check_api_availability():
-            input("Нажмите Enter для выхода...")
+            input(Fore.RED + "Нажмите Enter для выхода..." + Style.RESET_ALL)
             return
 
         selector = EpisodeSelector(api)
         season, selected_episode, english_title, selected_quality, selected_dub, selected_subs = selector.select_episode()
 
         if not season or not selected_episode or not english_title or not selected_quality or not selected_dub:
-            input("Нажмите Enter для выхода...")  # Ожидание ввода перед закрытием
+            input(Fore.RED + "Нажмите Enter для выхода..." + Style.RESET_ALL)  # Ожидание ввода перед закрытием
             return
 
         if not download_video(season, selected_episode['localId'], selected_quality):
-            print("Ошибка при скачивании видео.")
+            print(Fore.RED + "Ошибка при скачивании видео." + Style.RESET_ALL)
             return
 
         if not download_audio(season, selected_episode['localId'], selected_dub['code']):
-            print("Ошибка при скачивании аудио.")
+            print(Fore.RED + "Ошибка при скачивании аудио." + Style.RESET_ALL)
             return
 
         if selected_subs and not download_subs(season, selected_episode['localId'], selected_subs['code']):
-            print("Ошибка при скачивании субтитров.")
+            print(Fore.RED + "Ошибка при скачивании субтитров." + Style.RESET_ALL)
             return
 
-        video_filename = os.path.join(TEMP_DIR, "video.mp4") if int(selected_quality) <= 1080 else os.path.join(TEMP_DIR,
-                                                                                                               "video.webm")
+        video_filename = os.path.join(TEMP_DIR, "video.mp4") if int(selected_quality) <= 1080 else os.path.join(TEMP_DIR, "video.webm")
         audio_filename = os.path.join(TEMP_DIR, "audio.opus")
         subs_filename = os.path.join(TEMP_DIR, "subs.ass") if selected_subs else None
         output_filename = os.path.join(OUTPUT_DIR, f"{selected_episode['title']}.mkv")
 
         if not merge_video_audio_subs(video_filename, audio_filename, subs_filename, output_filename):
-            print("Ошибка при объединении видео.")
+            print(Fore.RED + "Ошибка при объединении видео." + Style.RESET_ALL)
             return
 
         cleanup_temp_files()
 
-        print(f"\nСерия {selected_episode['title']} успешно скачана и объединена!\n"
-                           f"Ты можешь найти ее в папке {OUTPUT_DIR}")
+        print(Fore.MAGENTA + "\nСерия" + Fore.CYAN + f"{selected_episode['title']} успешно скачана и объединена!\n" + Fore.MAGENTA +
+                           "Ты можешь найти ее в директории загрузок" + Fore.BLUE + f"{OUTPUT_DIR}" + Style.RESET_ALL)
+
+        os.startfile(OUTPUT_DIR)
 
     except Exception as e:
-        logging.error(f"Произошла ошибка: {e}")
-    input("Нажмите Enter для выхода...")
+        logging.error(Fore.RED + f"Произошла ошибка: {e}" + Style.RESET_ALL)
+    input(Fore.RED + "Нажмите Enter для выхода..." + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
